a95a0b4686f5e67f8de156e33b355cbbde2011c3
Duckers <198241505+TheRealDuckers@users.noreply.github.com>
2026-01-04 10:31:16 +0000

    Update useServices.tsx

--- src/services/hooks/useServices.tsx
+++ src/services/hooks/useServices.tsx
@@ -46,7 +46,7 @@ function useServices() {
 }
 
 async function logs(key: string): Promise<LogDaySummary[]> {
-    const response = await fetch(`https://raw.githubusercontent.com/TheRealDuckers/stauts/main/public/status/${key}_report.log`);
+    const response = await fetch(`https://raw.githubusercontent.com/TheRealDuckers/status/main/public/status/${key}_report.log`);
 
     const text = await response.text();
     const lines = text.split("\n");
